1afd79aa0b9fa74f57e80899f8276bab3ee779a4
Lukas Vaclavek <poortrolls@gmail.com>
2019-05-10 10:07:29 +0200

    petsitteroffer

--- petnb.DTL/Data/Models/PetSitterOffer.cs
+++ petnb.DTL/Data/Models/PetSitterOffer.cs
@@ -12,10 +12,11 @@ namespace petnb.DTL.Data.Models
         public string Heading { get; set; }
         public string Content { get; set; }
         public string Location { get; set; }
-        public int PetType { get; set; }
-        public string PetBreed { get; set; }
+     //   public int PetType { get; set; }
+    //    public string PetBreed { get; set; }
         public DateTime StartOfSit { get; set; }
         public DateTime EndOfSit { get; set; }
         public ApplicationUser User { get; set; }
+        public int ExpectedSalary { get; set; }
     }
 }
