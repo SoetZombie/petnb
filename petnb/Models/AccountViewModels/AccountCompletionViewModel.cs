97d0196f59cb540be561898529cfbe72532a9040
Maureen <Maureenkoopman9@gmail.com>
2019-05-24 11:30:04 +0200

    commitshit

--- petnb/Models/AccountViewModels/AccountCompletionViewModel.cs
+++ petnb/Models/AccountViewModels/AccountCompletionViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -8,14 +9,24 @@ namespace petnb.Models.AccountViewModels
 {
     public class AccountCompletionViewModel
     {
+        //Required = validation
+        [Required]
         public string FullName { get; set; }
+        [Required]
         public DateTime Birthdate { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public int Zipcode { get; set; }
+        [Required]
         public IFormFile ProfileImage { get; set; }
+        [Required]
         public bool WasPetSitter { get; set; }
+        [Required]
         public PetSitterExperienceEnum PetSitterExperienceEnum { get; set; }
+        [Required]
         public string PetSitterExperience { get; set; }
+        [Required]
         public string Bio { get; set; }
     }
 }
